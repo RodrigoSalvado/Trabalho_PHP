3706c6200ad1c5f3b7215824e831eec703155e3c
Kirlitos <rodrigoapsalvado@gmail.com>
2024-05-02 19:30:02 +0100

    Update à bd, criacao do admin, docente, aluno

--- basedados/criar_bd.sql
+++ basedados/criar_bd.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 18-Abr-2024 às 22:11
--- Versão do servidor: 10.4.28-MariaDB
--- versão do PHP: 8.2.4
+-- Host: 127.0.0.1
+-- Tempo de geração: 02-Maio-2024 às 20:25
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `TrabalhoLPI`
+-- Banco de dados: `trabalholpi`
 --
 
     CREATE DATABASE TrabalhoLPI;
@@ -30,8 +30,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `curso` (
-  `id_curso` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL
+                         `id_curso` int(11) NOT NULL,
+                         `nome` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -41,8 +41,8 @@ CREATE TABLE `curso` (
 --
 
 CREATE TABLE `tipo_utilizador` (
-  `id` int(11) NOT NULL,
-  `cargo` varchar(50) NOT NULL
+                                   `id` int(11) NOT NULL,
+                                   `cargo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -50,10 +50,11 @@ CREATE TABLE `tipo_utilizador` (
 --
 
 INSERT INTO `tipo_utilizador` (`id`, `cargo`) VALUES
-(1, 'cliente'),
-(2, 'aluno'),
-(3, 'docente'),
-(4, 'administrador');
+                                                  (1, 'cliente'),
+                                                  (2, 'aluno'),
+                                                  (3, 'docente'),
+                                                  (4, 'administrador'),
+                                                  (5, 'apagado');
 
 -- --------------------------------------------------------
 
@@ -62,13 +63,21 @@ INSERT INTO `tipo_utilizador` (`id`, `cargo`) VALUES
 --
 
 CREATE TABLE `utilizador` (
-  `id_utilizador` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `telemovel` varchar(20) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `tipo_utilizador` int(11) NOT NULL
+                              `id_utilizador` int(11) NOT NULL,
+                              `username` varchar(50) NOT NULL,
+                              `password` varchar(100) NOT NULL,
+                              `email` varchar(150) NOT NULL,
+                              `tipo_utilizador` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `utilizador`
+--
+
+INSERT INTO `utilizador` (`id_utilizador`, `username`, `password`, `email`, `tipo_utilizador`) VALUES
+                                                                                                   (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com', 4),
+                                                                                                   (2, 'docente', 'ac99fecf6fcb8c25d18788d14a5384ee', 'docente@docente.com', 3),
+                                                                                                   (3, 'aluno', 'ca0cd09a12abade3bf0777574d9f987f', 'aluno@aluno.com', 2);
 
 --
 -- Índices para tabelas despejadas
@@ -78,19 +87,19 @@ CREATE TABLE `utilizador` (
 -- Índices para tabela `curso`
 --
 ALTER TABLE `curso`
-  ADD PRIMARY KEY (`id_curso`);
+    ADD PRIMARY KEY (`id_curso`);
 
 --
 -- Índices para tabela `tipo_utilizador`
 --
 ALTER TABLE `tipo_utilizador`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `utilizador`
 --
 ALTER TABLE `utilizador`
-  ADD PRIMARY KEY (`id_utilizador`);
+    ADD PRIMARY KEY (`id_utilizador`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -100,19 +109,19 @@ ALTER TABLE `utilizador`
 -- AUTO_INCREMENT de tabela `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `tipo_utilizador`
 --
 ALTER TABLE `tipo_utilizador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `utilizador`
 --
 ALTER TABLE `utilizador`
-  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
