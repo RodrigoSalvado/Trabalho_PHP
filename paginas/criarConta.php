584959bbd868116ee4ed34bd6682f2260bc94b3a
Kirlitos <rodrigoapsalvado@gmail.com>
2024-05-02 19:04:13 +0100

    Criacao de Conta

--- paginas/criarConta.php
+++ paginas/criarConta.php
@@ -1,12 +1,12 @@
 <?php
+global $conn;
+include '../basedados/basedados.h';
 
 $user = $_POST["user"];
 $pass = $_POST["pass"];
-$tml = $_POST["tml"];
 $email = $_POST["email"];
 
-$sql = "INSERT INTO utilizador (username, password, telemovel, email) 
-                    VALUES ('".$user."',".md5($pass)."','".$tlm."', '".$email."');";
+$sql = "INSERT INTO `utilizador`(`username`, `password`, `email`) VALUES ('$user','".md5($pass)."','$email');";
 //echo $sql;
 $res = mysqli_query ($conn, $sql);
-header ("Location:./paginaPrincipal.htlm");
+header ("Location:./paginaPrincipal.html");
