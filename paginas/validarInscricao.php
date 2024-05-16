763f9ae8900bcbb66336718ad4f6b84891753b40
Kirlitos <rodrigoapsalvado@gmail.com>
2024-05-16 23:36:13 +0100

    update validarInscricao.php

--- paginas/validarInscricao.php
+++ paginas/validarInscricao.php
@@ -2,12 +2,16 @@
 global $conn;
 include "../basedados/basedados.h";
 
-$id = $_GET["id"];
-$validado = $_GET["validar"];
-$curso = $_GET["curso"];
+try{
+    $id = $_GET["id"];
+    $validado = $_GET["validar"];
+    $curso = $_GET["curso"];
+}catch (Exception $e){
+
+}
 
 if($validado == 1){
-    $sql = "UPDATE util_curso SET aceite = 1";
+    $sql = "UPDATE util_curso SET aceite = 1 WHERE curso = $curso";
     mysqli_query($conn, $sql);
     header("Location: gestaoInscricoes.php");
 
