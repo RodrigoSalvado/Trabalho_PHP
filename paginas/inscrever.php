066ec7f94e10531e8493b22a3bf33c49e6e8e655
Kirlitos <rodrigoapsalvado@gmail.com>
2024-05-18 01:22:18 +0100

    So é possivel inscrever uma vez num curso

--- paginas/inscrever.php
+++ paginas/inscrever.php
@@ -8,6 +8,7 @@ $username = $_SESSION["user"] ;
 $nomeCurso = $_POST['nome'];
 echo $nomeCurso;
 
+
 $sql = "SELECT id_utilizador FROM utilizador WHERE username = '$username'";
 $result = mysqli_query($conn, $sql);
 if($result && mysqli_num_rows($result) > 0){
@@ -16,19 +17,29 @@ if($result && mysqli_num_rows($result) > 0){
 }else{
    echo "erro select";
 }
-
-
-
 echo $id_user;
 
-$sqlInsert = "INSERT INTO util_curso (id_utilizador, curso, aceite) VALUES ('$id_user', '$nomeCurso', 0)";
-$resultInsert = mysqli_query($conn, $sqlInsert);
+$sqlVerf = "SELECT *
+            FROM util_curso
+            WHERE id_utilizador = '$id_user'
+            AND curso = '$nomeCurso'";
+$resultVerf = mysqli_query($conn, $sqlVerf);
 
-if($resultInsert){
-    echo "<script>window.alert('Muito obrigado!\\nAguarde até que a sua inscrição seja validada!') ;</script>";
+if(mysqli_num_rows($resultVerf) == 0){
+        $sqlInsert = "INSERT INTO util_curso (id_utilizador, curso, aceite) VALUES ('$id_user', '$nomeCurso', 0)";
+        $resultInsert = mysqli_query($conn, $sqlInsert);
+
+        if($resultInsert){
+            echo "<script>window.alert('Muito obrigado!\\nAguarde até que a sua inscrição seja validada!') ;</script>";
+        }else{
+            echo "<script>window.alert('erro') ;</script>";
+        }
 }else{
-    echo "<script>window.alert('erro') ;</script>";
+    echo "<script>alert('Já fez uma inscrição para este curso!')</script>";
 }
+
+
+
 
 
 ?>
